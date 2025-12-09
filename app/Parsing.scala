115462638ced2b9a2edd8ed0ba65f8bc0ab9cc82
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2025-12-10 00:42:16 +0200

    parse date from nimenhuuto format

--- app/Parsing.scala
+++ app/Parsing.scala
@@ -1,6 +1,28 @@
+import java.time.LocalDateTime
+
 import net.ruippeixotog.scalascraper.dsl.DSL.*
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract.*
 import net.ruippeixotog.scalascraper.model.Document
+
+def parseDate(dateStr: String, defaultYear: Int): Option[LocalDateTime] =
+  dateStr match
+    case s"$_ $day.$month. klo $hour:$minute" =>
+      toLocalDateTime(defaultYear.toString, month, day, hour, minute)
+
+    case s"$_ $day.$month.$year klo $hour:$minute" =>
+      toLocalDateTime(year, month, day, hour, minute)
+
+    case _ =>
+      None
+
+def toLocalDateTime(yearStr: String, monthStr: String, dayStr: String, hourStr: String, minuteStr: String) =
+  for
+    year   <- yearStr.toIntOption
+    month  <- monthStr.toIntOption
+    day    <- dayStr.toIntOption
+    hour   <- hourStr.toIntOption
+    minute <- minuteStr.toIntOption
+  yield LocalDateTime.of(year, month, day, hour, minute)
 
 def parsePlayers(eventDoc: Document): Players =
   val inPlayersId      = "zone_1"
--- app/project.scala
+++ app/project.scala
@@ -1,2 +1,3 @@
 //> using scala "3.3.7"
 //> using dep "net.ruippeixotog::scala-scraper:3.2.0"
+//> using test.dep "org.scalameta::munit:1.2.1"
