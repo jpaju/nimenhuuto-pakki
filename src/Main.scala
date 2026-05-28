cff5fc9970a4263dca77b24a45cfbbe689302769
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2026-05-28 20:10:31 +0300

    allow showing help without setting environment vars

--- src/Main.scala
+++ src/Main.scala
@@ -2,16 +2,9 @@ import com.monovore.decline.*
 
 @main
 def main(args: String*): Unit =
-  val baseUrl   = requireEnvVar("BASE_URL")
-  val sessionId = requireEnvVar("NIMENHUUTO_SESSION_ID")
-
-  val client  = LiveNimenhuutoClient(baseUrl, sessionId)
-  val service = NimenhuutoService(client)
-  val app     = Application(service)
-
   CliCommand.main.parse(args) match
     case Left(help) => showHelp(help)
-    case Right(cmd) => runCommand(app)(cmd)
+    case Right(cmd) => runCommand(cmd)
 
 private def showHelp(help: Help) =
   val (output, exitCode) = help.errors match
@@ -21,11 +14,19 @@ private def showHelp(help: Help) =
   output.println(help)
   sys.exit(exitCode)
 
-private def runCommand(app: Application): CliCommand => Unit =
-  case CliCommand.ListEvents(filter)      => app.listEvents(filter)
-  case CliCommand.CountAttendance(filter) => app.countAttendance(filter)
-  case CliCommand.EventHistory(filter)    => app.eventHistory(filter)
-  case CliCommand.ShowRoster              => app.showRoster()
+private def runCommand(cmd: CliCommand) =
+  val baseUrl   = requireEnvVar("BASE_URL")
+  val sessionId = requireEnvVar("NIMENHUUTO_SESSION_ID")
+
+  val client  = LiveNimenhuutoClient(baseUrl, sessionId)
+  val service = NimenhuutoService(client)
+  val app     = Application(service)
+
+  cmd match
+    case CliCommand.ListEvents(filter)      => app.listEvents(filter)
+    case CliCommand.CountAttendance(filter) => app.countAttendance(filter)
+    case CliCommand.EventHistory(filter)    => app.eventHistory(filter)
+    case CliCommand.ShowRoster              => app.showRoster()
 
 private def requireEnvVar(name: String): String =
   sys.env.get(name).getOrElse {
